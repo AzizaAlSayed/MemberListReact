f2b0b82563e006d6dc852d290827c35ec28381d8
lara <azizasayedahmad@gmail.com>
2021-11-22 15:17:49 +0200

    member-list-component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import Header from './header';
 import Search from './search.js';
-import MemberList from './memberList.js';
+import Member from './member.js';
 import NewMember from './newMember.js';
-
 import '../App.css';
 
 function App() {
@@ -12,13 +11,7 @@ function App() {
         <Header />
         <div className="TitleStyle">
           <Search />
-          <MemberList name="Carolien Bloeme" />
-          <MemberList name="Sun Jun" />
-          <MemberList name="Song Bao" />
-          <MemberList name="Olivia Arribas" />
-          <MemberList name="Bonginkosi Mdladlana" />
-          <MemberList name="Ariana Belomestnykh" />
-          <MemberList name="Jacqueline Likoki" />
+          <Member />
           <NewMember />
         </div>
       </div>
