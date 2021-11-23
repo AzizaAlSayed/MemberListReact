ce0da9b00ff5bc6dbf6c0518331412747c4e6ff2
lara <azizasayedahmad@gmail.com>
2021-11-23 15:27:10 +0200

    search feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,26 @@
 import Header from './header';
-import Search from './search.js';
-import MemberList from './memberList.js';
-import NewMember from './newMember.js';
-
+import Search from './search';
+import NewMember from './newMember';
 import '../App.css';
+import { useState } from "react";
+import { members } from '../data.js';
+import MemberList from './memberList';
 
 function App() {
+  const [memberName, setMemberName] = useState("");
+
+  const searchMemberResult = !memberName
+    ? members : members.filter(member =>
+      (member.name.toLowerCase()).includes(memberName.toLowerCase())
+    );
   return (
     <div className="Container">
       <div className="LeftPadding" >
         <Header />
         <div className="TitleStyle">
-          <Search />
-          <MemberList name="Carolien Bloeme" />
-          <MemberList name="Sun Jun" />
-          <MemberList name="Song Bao" />
-          <MemberList name="Olivia Arribas" />
-          <MemberList name="Bonginkosi Mdladlana" />
-          <MemberList name="Ariana Belomestnykh" />
-          <MemberList name="Jacqueline Likoki" />
+          <Search memberName={memberName}
+            onMemberNameChange={setMemberName} />
+          <MemberList members={searchMemberResult} />
           <NewMember />
         </div>
       </div>
