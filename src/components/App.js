47dd178f6e48077b6d51239490b5e14961bfa996
lara <azizasayedahmad@gmail.com>
2021-11-24 19:56:33 +0200

    maintained toggle

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import Header from './header';
 import Search from './search.js';
 import MemberList from './memberList.js';
 import NewMember from './newMember.js';
+import { members } from '../data';
 
 import '../App.css';
 
@@ -12,13 +13,7 @@ function App() {
         <Header />
         <div className="TitleStyle">
           <Search />
-          <MemberList name="Carolien Bloeme" />
-          <MemberList name="Sun Jun" />
-          <MemberList name="Song Bao" />
-          <MemberList name="Olivia Arribas" />
-          <MemberList name="Bonginkosi Mdladlana" />
-          <MemberList name="Ariana Belomestnykh" />
-          <MemberList name="Jacqueline Likoki" />
+          <MemberList members={members} />
           <NewMember />
         </div>
       </div>
