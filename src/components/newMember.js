876a122570f93ee1b90791898af5caad9aa85e03
lara <azizasayedahmad@gmail.com>
2021-11-24 17:22:23 +0200

    disabled empty member input

--- src/components/newMember.js
+++ src/components/newMember.js
@@ -11,7 +11,7 @@ export default function NewMember({ onAddNewMember }) {
                         onChange={memberName => setName(memberName.target.value)} />
                 </div>
             </div>
-            <button type="reset" name="learn" className="ButtonAddMember" value="myimage" onClick={() => { setName(''); onAddNewMember(name); }}>
+            <button disabled={name === "" || name === null} type="reset" name="learn" className="ButtonAddMember" value="myimage" onClick={() => { setName(''); onAddNewMember(name); }}>
                 <svg version="1.1" width="10" height="10" id="Capa_1" xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 490 490"
                     fill="white" space="preserve">
                     <polygon points="222.031,490 267.969,490 267.969,267.969 490,267.969 490,222.031 267.969,222.031 267.969,0 222.031,0 
